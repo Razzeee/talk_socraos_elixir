fea349b6a631fc31fcc2c570f0ed6df9c13c67e0
hlhr <henning.loehr@gmail.com>
2019-02-27 18:12:26 +0100

    add example for recursion

---
+++ demo_syntax/3_recursion.exs
@@ -0,0 +1,19 @@
+# elixir 3_recursion.exs
+
+defmodule Math do
+  def sum(values), do: sum(values, 0)
+
+  # def sum(values, accumulator \\ 0)
+
+  defp sum([head | tail], accumulator) do
+    sum(tail, head + accumulator)
+  end
+
+  defp sum([], accumulator) do
+    accumulator
+  end
+end
+
+IO.inspect(Math.sum([1, 2, 3, 4, 5, 6]))
+
+# IO.inspect(Enum.sum([1, 2, 3, 4, 5, 6]))
